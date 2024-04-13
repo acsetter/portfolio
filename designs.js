2b708656156154d9709c5ce04332de6e68356deb
Aaron Csetter <aaron@csetter.com>
2024-04-13 11:29:52 -0400

    revamp spring 2024

--- designs.js
+++ designs.js
@@ -1,15 +1,26 @@
 import {Tile, Tilestream} from "./tilestream.js";
 
 let axonTiles = [
+    new Tile('img/pawpals/splash.png', '', ''),
+    new Tile('img/pawpals/SignUp_500px.png', '', ''),
+    new Tile('img/pawpals/Recovery_800px.png', '', ''),
+    new Tile('img/ui/AUDIT_OL.jpg', '', ''),
+    new Tile('img/ui/ms/hard.png', '', ''),
+    new Tile('img/ui/ms/help.png', '', ''),
+    new Tile('img/pawpals/AppFlowChart5.png', '', ''),
+    new Tile('img/pawpals/TechStack.png', '', ''),
     new Tile('img/axon/axon_12.png', '', ''),
     new Tile('img/axon/axon_2.png', '', ''),
+    new Tile('img/lg/AXON_COMP.png', '', ''),
+    new Tile('img/lg/LG_WALL2.png', '', ''),
+    new Tile('img/lg/LG_WALL3.png', '', ''),
+    new Tile('img/lg/LG_VIK.png', '', ''),
     new Tile('img/axon/axon_3.png', '', ''),
     new Tile('img/axon/axon_4.png', '', ''),
     new Tile('img/axon/axon_11.png', '', ''),
     new Tile('img/axon/AXON_FA.png', '', ''),
     new Tile('img/axon/AXON_BBC.png', '', ''),
     new Tile('img/axon/axon_10.png', '', ''),
-    new Tile('img/axon/axon_15.jpg', '', ''),
     new Tile('img/axon/axon_16.png', '', ''),
     new Tile('img/axon/axon_17.png', '', ''),
     new Tile('img/axon/axon_18.png', '', ''),
@@ -33,6 +44,9 @@ let axonTiles = [
 ]
 
 let stream = new Tilestream(axonTiles);
-stream.desktop(4, 6.5, 5);
+stream.desktop(6, 14, 5);
+stream.tablet(1750, 4, 8, 5);
+stream.mobile(800, 3, 5, 5);
 
-stream.render(document.getElementById('design'));
+
+stream.render(document.getElementById('tilestream'));
